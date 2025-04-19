57fb3b1bba076eea4b3abdaea59ad7f272755d7f
RaillyLinker <raillylinker@gmail.com>
2025-04-19 15:37:37 +0900

    refactor : 설정 정보 분리

---
+++ module_sample_api/configurations/app_conf.py
@@ -0,0 +1,14 @@
+class AppConf:
+    # controllers 폴더 위치(main.py 기중)
+    controllers_package_name = "controllers"
+
+    # 서버가 어떤 IP 주소에서 접근을 허용할지를 설정하는 옵션
+    # "127.0.0.1" 또는 "localhost"	로컬 컴퓨터에서만 접근 가능 (외부에서 접근 불가)
+    # "0.0.0.0"	모든 IP에서 접근 허용 (같은 네트워크의 다른 장치나 외부에서도 접속 가능)
+    unicorn_host = "0.0.0.0"
+
+    # 서버 점유 포트
+    unicorn_port = 8080
+
+    # 코드를 수정 하면 서버를 자동으로 재시작(reload) 해주는 개발용 기능
+    unicorn_reload = True
--- module_sample_api/main.py
+++ module_sample_api/main.py
@@ -2,20 +2,18 @@ import importlib
 import pkgutil
 from fastapi import FastAPI, APIRouter
 import uvicorn
+from configurations.app_conf import AppConf
 
 # [FastAPI 실행 Main]
 # FastAPI 객체 생성
 app = FastAPI()
 
-# Controllers 경로 지정
-package_name = "controllers"
-
 # controllers 디렉토리에 있는 모든 라우터 등록
-for _, module_name, _ in pkgutil.iter_modules([package_name]):
-    module = importlib.import_module(f"{package_name}.{module_name}")
+for _, module_name, _ in pkgutil.iter_modules([AppConf.controllers_package_name]):
+    module = importlib.import_module(f"{AppConf.controllers_package_name}.{module_name}")
     if hasattr(module, "router") and isinstance(module.router, APIRouter):
         app.include_router(module.router)
 
 # FastAPI 서버 실행 (포트 8080으로 설정)
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host=AppConf.unicorn_host, port=AppConf.unicorn_port, reload=AppConf.unicorn_reload)
