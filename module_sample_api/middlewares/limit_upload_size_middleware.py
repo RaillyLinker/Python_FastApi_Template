650d0806ed364f832c8558e65cc86ac736a8f40e
RaillyLinker <raillylinker@gmail.com>
2025-04-23 09:00:25 +0900

    docs : 주의사항 추가

--- module_sample_api/middlewares/limit_upload_size_middleware.py
+++ module_sample_api/middlewares/limit_upload_size_middleware.py
@@ -5,6 +5,8 @@ import module_sample_api.configurations.app_conf as app_conf
 
 
 # [파일 업로드 사이즈 제한 미들웨어]
+# 클라이언트 측은 전송하는 데이터 스트림을 전부 비운 후 응답을 받기에 응답이 느릴 수도 있음.
+# 고로 이 미들웨어는 서버측 보호를 위한 안전장치이고, 실제 파일 업로드 컷은 클라이언트 측 로직에서 처리하도록 할 것.
 class LimitUploadSizeMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         content_length = request.headers.get("Content-Length")
